3697d7a37fb9b299989adf3d14ba0d9092af1951
codenameabel <newabelmartinez@gmail.com>
2021-08-26 14:43:00 -0700

    deploying again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-searchgit ', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
