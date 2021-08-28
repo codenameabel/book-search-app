0fadd91015c5f9a5ada329d503fb1a7b4a04f4af
codenameabel <newabelmartinez@gmail.com>
2021-08-27 18:50:52 -0700

    sready for deployment again

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -15,13 +15,13 @@ const AppNavbar = () => {
       <Navbar bg='dark' variant='dark' expand='lg'>
         <Container fluid>
           <Navbar.Brand as={Link} to='/'>
-            Books Galore
+            Book Search
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='navbar' />
           <Navbar.Collapse id='navbar'>
             <Nav className='ml-auto'>
               <Nav.Link as={Link} to='/'>
-                Search For Books
+                Search Here
               </Nav.Link>
               {/* if user is logged in show saved books and logout */}
               {Auth.loggedIn() ? (
